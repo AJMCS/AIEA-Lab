575c5e993ff76480acdc98650743ef011b6b55d3
AJ McDaniel <auston.mcdaniel7@gmail.com>
2026-03-04 11:54:58 -0800

    Task 2 experiment

---
+++ Task 02 experiment/experiment.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+message = input("Start you conversation which chatGPT (Type \'quit\' to stop):\n\n")
+
+while(message != "quit"):
+
+    response = client.responses.create(
+        model="gpt-5.2",
+        reasoning={"effort":"medium"},
+        instructions="You are an expert software developer with a specialization in swi-prolog and traditional data structures and algorithms concepts. " \
+        "Your specialties lies in the translation of this into swi-prolog specifically.",
+        input=message
+    )
+
+    print(response + "\n\n")
+
+    message = input()
+
